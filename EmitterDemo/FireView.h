f0312fc49f1ccf0a7aaa35d2148e7c9333cc4ed3
c_calary <229336009@qq.com>
2017-06-14 15:52:15 +0800

    iOS 粒子动画

---
+++ EmitterDemo/FireView.h
@@ -0,0 +1,13 @@
+//
+//  FireView.h
+//  EmitterDemo
+//
+//  Created by caohouhong on 17/6/14.
+//  Copyright © 2017年 caohouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FireView : UIView
+- (void) startAnimation;
+@end
